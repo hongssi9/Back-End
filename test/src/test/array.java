7534596a0bb2f6854d73920a08b7de45ce8a30bf
Hongssi <vvv9605@gmail.com>
2021-03-11 00:12:34 +0900

    연습용

---
+++ test/src/test/array.java
@@ -0,0 +1,21 @@
+package test;
+
+public class array {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		//배열 크기 할당
+		//int[] arr = new int[10];
+		String[] arr2 = new String[20];
+		
+		//arr = new int[5]; //arr배열을 5의크기로 초기화
+		
+		int[] arr = {1,2,3,4,5};
+		for(int i=0; i<arr.length;i++) {
+			System.out.println(arr[i]);
+		}
+		
+	}
+
+}
