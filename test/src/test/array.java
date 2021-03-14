bfa52ebbf9b2a3592b942e8f27425557af610ea2
hongssi9 <79443935+hongssi9@users.noreply.github.com>
2021-03-14 23:32:36 +0900

    혼자 연습한거

--- test/src/test/array.java
+++ test/src/test/array.java
@@ -4,21 +4,23 @@ public class array {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		// 1. 정수 10개짜리 nums 배열을 생성한다.
-				
-
-				// 2. nums에 숫자를 2,4,5,6... 처럼 짝수로 10개를 채우는 반복문을 작성한다.
-//				for (int i = 0; i < 10; i++)
-//					nums[i] = 2 * (i + 1);
-
-				// 2. nums에 숫자를 1,2,3,1,2,3,1,2,3,1 처럼 1,2,3이 순환되는 값으로 채우는 반복문
-			
-
-				// 3.num을 출력하는 코드를 작성한다
-				// 출력하는 모양은 다음과 같다.
-				// 2,3,4,5,6,7,9,...20
+//		// 1. 정수 10개짜리 nums 배열을 생성한다.
+//				int[] nums = new int[10];
+//
+//				// 2. nums에 숫자를 2,4,5,6... 처럼 짝수로 10개를 채우는 반복문을 작성한다.
+//				for(int i=0; i<nums.length;i++) {
+//					nums[i] = (i+1)*2;
+//				}
+//				for(int i=0; i<nums.length; i++) {
+//					
+//					System.out.printf("%d,",nums[i]);
+//				}
+//			
+//
+//				// 3.num을 출력하는 코드를 작성한다
+//		
+//	
 		
-	
 		
 	}
 
