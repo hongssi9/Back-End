3df66658f4d40d539311365b41c9f411b8082ef5
hongssi9 <vvv9605@naver.com>
2021-03-14 23:30:38 +0900

    03-14 혼자 연습

--- test/src/test/InOut.java
+++ test/src/test/InOut.java
@@ -1,10 +1,28 @@
 package test;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Scanner;
+
 public class InOut {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
-
+		
+//		//1. names텍스트를 가져와서 배열에 넣고 ,로 구분하여 유재석만 출력하시오
+			
+		FileInputStream text = new FileInputStream("res/names.txt");
+		Scanner sc = new Scanner(text);
+		
+		String line = sc.nextLine();
+		
+		String[] names = line.split(",");
+		
+		System.out.println(names[0]);
+		
+		
+		
+			
 	}
-
 }
+
--- test/src/test/scanner.java
+++ test/src/test/scanner.java
@@ -5,22 +5,35 @@ import java.util.Scanner;
 public class scanner {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		Scanner sc = new Scanner(System.in);
-		int kor, eng, math;
-		String name;
+		int kor, math, eng, total;
+		double avg;
+		Scanner sc = new Scanner(System.in); //System.in은 화면에서 출력을 받겠다는 뜻.
 		
-		System.out.print("이름을 입력하세요");
-		name=sc.nextLine();
-		System.out.print("국어");
-		kor=sc.nextInt();
-		System.out.print("영어");
-		eng=sc.nextInt();
+		System.out.println("-----------성적 입력-------------");
+		System.out.println("국어점수");
+		kor = sc.nextInt();
+		System.out.println("수학점수");
+		math = sc.nextInt();
+		System.out.println("영어점수");
+		eng = sc.nextInt();
 		
-		System.out.printf("이름:%s\n",name);
-		System.out.printf("국어:%d\n",kor);
-		System.out.printf("영어:%d\n",eng);
+		total = kor+math+eng;
+		avg = total/3;
 		
+		
+		System.out.println("-----------성적 출력-------------");
+		System.out.printf("국어점수 : %d",kor);
+		System.out.printf("수학점수 : %d",math);
+		System.out.printf("영어점수 : %d",eng);
+		System.out.printf("총합 : %d",total);
+		System.out.printf("평균 : %s",avg);
+		
+		
+		
+		
+		
+		
+	
 		
 		
 		
