ddfcc7dd42602aba96150f508a0cfce6d0f0b6a3
Hongssi <vvv9605@gmail.com>
2021-03-23 18:25:11 +0900

    함수 만들기

---
+++ src/ex5/fnc/LottoProgram.java
@@ -0,0 +1,36 @@
+package ex5.fnc;
+
+public class LottoProgram {
+
+	public static void main(String[] args) {
+		//로또를 만들자
+		int[] lotto;
+		
+		//로또를 출력하자
+		lotto = gen();
+		
+		//로또를 정렬하자
+		sort(lotto);
+		
+		//로또를 출력하자
+		print(lotto);
+	}
+
+	private static void sort(int[] lotto) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private static void print(int[] lotto) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	private static int[] gen() {
+		// TODO Auto-generated method stub
+		int[] lotto = new int[6];
+		
+		return lotto;
+	}
+
+}
