37992eeeee85568f89a31eb0fc4436a60bf87298
hongssi9 <vvv9605@naver.com>
2021-03-29 00:10:24 +0900

    캡슐화

--- src/캡슐화로또/Lotto.java
+++ src/캡슐화로또/Lotto.java
@@ -6,66 +6,66 @@ import java.util.Random;
 public class Lotto {
 	private int[] nums; // 로또 번호를 정의함
 	private int size;
+
 	
-	public static void init(Lotto lotto) { //반환 없는 함수
+	//---------------------------초기화------------------------------------------
+	public static void init(Lotto lotto) { // 반환 없는 함수 //구현부
 		lotto.size = 6;
 		lotto.nums = new int[lotto.size];
 	}
 	
+	
+	
+	//-----------------------------랜덤으로 숫자 생성-----------------------------------------
+		static Lotto gen(Lotto lotto) {
+//			Lotto lotto = new Lotto(); // 4byte 이 객체를 프로그램 클래스에서 공유한다
+//			lotto.nums = new int[6]; // 24byte
+
+			Random rand = new Random();
+
+			for (int i = 0; i < lotto.size; i++) {
+				lotto.nums[i] = rand.nextInt(45) + 1;
+
+			}
+
+			return lotto;
+		}
+		
+		
+		//-------------------------------------------정렬--------------------------------------
+		public static void sort(Lotto lotto) {
+
+			for (int j = 0; j < 5; j++) {
+				for (int i = 0; i < 5 - j; i++) {
+					int compare;
+					if (lotto.nums[i] > lotto.nums[i + 1]) {
+						compare = lotto.nums[i];
+						lotto.nums[i] = lotto.nums[i + 1];
+						lotto.nums[i + 1] = compare;
+					}
+				}
+			}
+		}
+	
+	
+
+	//-------------------------------for문에 돌릴 사이즈-------------------------------
 	public static int getSize(Lotto lotto) {
 		return lotto.size;
 	}
 	
 	
-	//static 은 함수를 만들때
+	
+//-------------------------------------------------------------------
+	// static 은 함수를 만들때
 	public static int getNum(Lotto lotto, int i) {
-//		return lotto.nums[i]; //로또에 nums에서 i번째
-		
 		int num = lotto.nums[i];
-		
+
 		return num;
 	}
-
-	static Lotto gen() {
-		Lotto lotto = new Lotto(); //4byte  이 객체를 프로그램 클래스에서 공유한다
-		lotto.nums = new int[6]; //24byte  
-
-		Random rand = new Random();
-
-		for (int i = 0; i < 6; i++) {
-			lotto.nums[i] = rand.nextInt(45) + 1;
-
-		}
-
-		return lotto;
-	}
-
-	public static void sort(Lotto lotto) {
-
-		for (int j = 0; j < 5; j++) {
-			for (int i = 0; i < 5 - j; i++) {
-				int compare;
-				if (lotto.nums[i] > lotto.nums[i + 1]) {
-					compare = lotto.nums[i];
-					lotto.nums[i] = lotto.nums[i + 1];
-					lotto.nums[i + 1] = compare;
-				}
-			}
-		}
-	}
-
-
 	
 	
 	
-	
-//	public int[] getNums() {
-//		return nums;
-//	}
-//	
-//	public int getNum(Lotto lotto, int i) {
-//		int num = lotto.nums[i];
-//		return num;
-//	}
+
 
 }
--- src/캡슐화로또/Program.java
+++ src/캡슐화로또/Program.java
@@ -3,6 +3,9 @@ package 캡슐화로또;
 import java.util.Random;
 
 public class Program {
+	
+	
+	//------------------------------출력----------------------------------
 	static void print(Lotto lotto) {
 		for (int i = 0; i < Lotto.getSize(lotto); i++) {
 			System.out.printf("[" + "%d" + "] ", Lotto.getNum(lotto, i));
@@ -10,39 +13,37 @@ public class Program {
 
 		System.out.println();
 	}
+	//프린트 함수니까 반환값이 없으니 메인함수 위에 메서드를 만들었다.
 	
 	
 
 	public static void main(String[] args) {
-//		Lotto lotto; //Lotto클래스를 사용함
+	
+		//1. lotto 객체를 생성
+		Lotto lotto = new Lotto();  //인스턴스화
+					
+		
+		//2. 초기화
+		Lotto.init(lotto);  //선언부  //반환타입.메서드이름(전달하려는 매개변수)
 
 		
-		//lotto 객체를 생성
-		Lotto lotto = new Lotto(); //4byte
-		//lotto를 생성 한 후에 반드시 초기화 해야 할 것.
-		//lotto.nums = new int[6]; //24byte
-		Lotto.init(lotto);
+		//3. 로또를 만들자
+		lotto = Lotto.gen(lotto); //Lotto의 인스턴스(객체)를 만드는 함수다.
 		
-//		int[] lotto;
-		// 로또를 만들자
+		
+		//4. 로또를 출력하자
+		print(lotto);
+		//lotto.print()
+		
+		
+		//5. 정렬
+		Lotto.sort(lotto);  //얘도 공유한다.  //정렬한다->로또를 Lotto안에 있는 sort함수를 이용해서
+		//lotto.sort();
+		
+		
+		//6. 다시 출력
+		print(lotto);
 
-		lotto = Lotto.gen(); //Lotto의 인스턴스(객체)를 만드는 함수다.
-		
-//		// 로또를 출력하자
-		print(lotto);
-		
-		Lotto.sort(lotto);  //얘도 공유한다.
-		
-		print(lotto);
-//		
-//		System.out.println();
-//		System.out.println("───────────after sort─────────── ");
-//
-//		// 로또를 정렬하자
-//		Lotto.sort(lotto);
-//
-//		// 로또를 출력하자
-//		print(lotto);
 	}
 
 
